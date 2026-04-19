c3d0b661b0fdf12e2ee9aaae91dc567f329e97f3
khushboo2289-del <khushboo.2289@gmail.com>
2026-04-19 09:49:11 +0530

    Update data_fetch.py

--- data_fetch.py
+++ data_fetch.py
@@ -23,8 +23,6 @@ def fetch_data():
             df["VolumeSpike"] = df["VolumeSpike"].fillna(0)
             df["MA20"] = df["Close"].rolling(20).mean()
 
-            df = df.dropna()
-
             latest = df.iloc[-1]
 
             data.append({
